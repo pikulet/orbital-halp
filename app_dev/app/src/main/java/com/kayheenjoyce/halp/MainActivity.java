40343ac354d6f144171026d13a6e53f74decc442
Joyce <jycyeo@yahoo.com.sg>
2018-06-15 11:16:56 +0800

    Developing the Full-Screen version of the prototype
    Included a new colour scheme

--- app_dev/app/src/main/java/com/kayheenjoyce/halp/MainActivity.java
+++ app_dev/app/src/main/java/com/kayheenjoyce/halp/MainActivity.java
@@ -1,18 +1,24 @@
 package com.kayheenjoyce.halp;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Random;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
 public class MainActivity extends AppCompatActivity {
+
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -35,6 +41,14 @@ public class MainActivity extends AppCompatActivity {
     private View mControlsView;
     private boolean mVisible;
 
+    // The current opening status of the clinic
+    protected static boolean clinicIsOpen = true;
+    protected static String CLINIC_OPEN;
+    protected static String CLINIC_CLOSED;
+
+    // The current estimated waiting time at the clinic. Provided by the clinic.
+    protected static int estimatedWaitingTime = Integer.MAX_VALUE;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
 
         mVisible = true;
         mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = findViewById(R.id.fullscreen_content);
+        mContentView = findViewById(R.id.fullscreen_content_controls);
 
 
         // Set up the user interaction to manually show or hide the system UI.
@@ -57,7 +71,11 @@ public class MainActivity extends AppCompatActivity {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+        findViewById(R.id.fullscreen_content_controls).setOnTouchListener(mDelayHideTouchListener);
+
+        // Use instance to link strings
+        CLINIC_OPEN = getString(R.string.main_clinic_status_open);
+        CLINIC_CLOSED = getString(R.string.main_clinic_status_closed);
     }
 
     @Override
@@ -164,5 +182,58 @@ public class MainActivity extends AppCompatActivity {
     private void delayedHide(int delayMillis) {
         mHideHandler.removeCallbacks(mHideRunnable);
         mHideHandler.postDelayed(mHideRunnable, delayMillis);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateClinicStatus(); // Updates to see if the clinic is opened or closed.
+        updateWaitingTime(); // Updates the clinic's estimated waiting time.
+    }
+
+    /**
+     * Updates the clinic status, if it is opened or not.
+     *      Currently, the clinic opening hours are set as:
+     *      Monday to Wednesday: 8.30am - 6.00pm
+     *      Thursday: 8.30am - 5.00pm
+     *      Friday: 8.30am - 5.30pm
+     *      We are closed on Saturday, Sunday & Public Holidays
+     *      Clinic is closed for lunch from 12.30pm - 1.30pm
+     *      Please note that last registration is 30 minutes prior to lunch or closing hours.
+     *
+     * In the future, this could be done by simply running an API call to the clinic
+     */
+    protected void updateClinicStatus() {
+        // Retrieves the date and time, checking if this is within the clinic's opening hours.
+        Calendar currentCalendar = Calendar.getInstance();
+        // TODO
+
+        String currentClinicStatus = clinicIsOpen ? CLINIC_OPEN : CLINIC_CLOSED;
+
+        // Updates the displayed text
+        TextView tView = findViewById(R.id.main_clinicStatus);
+        tView.setText(currentClinicStatus);
+    }
+
+    /**
+     * Updates the clinic's waiting time.
+     * Currently, this is done by generating a pseudorandom number from 45 to 60.
+     */
+    protected void updateWaitingTime() {
+        Random rand = new Random();
+        int randomWaitingTime = rand.nextInt(16) + 45;
+        MainActivity.estimatedWaitingTime = randomWaitingTime;
+
+        // Updates the displayed text
+        TextView tView = findViewById(R.id.main_estWaitTime);
+        tView.setText(String.valueOf(MainActivity.estimatedWaitingTime));
+    }
+
+    /**
+     * Starts a registration process by going to the next activity.
+     */
+    public void startRegistration(View view) {
+        Intent startReg = new Intent(this, RegistrationPage.class);
+        startActivity(startReg);
     }
 }
